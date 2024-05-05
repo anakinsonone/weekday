8ce23e02ccd08b7957aac2a4f17c27b0364e5d64
akhileshsonone <akkhi.sonone@gmail.com>
2024-05-06 00:16:59 +0530

    make labels visible only if any options are selected

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,6 +1,8 @@
 import Select from "react-select";
-
 import { Box, FormControl, Grid, Typography } from "@mui/material";
+
+import "./style.css";
+import { useState } from "react";
 
 export const Filter = ({
   title,
@@ -11,16 +13,25 @@ export const Filter = ({
   options: object[];
   multi: boolean;
 }) => {
+  const [selectedOptions, setSelectedOptions] = useState<any[]>([]);
+
   return (
-    <Grid item md={2}>
-      <FormControl>
+    <Grid item md={2} xs={5} sm={4} className="filter">
+      <FormControl fullWidth>
         <Box>
-          <Typography>{title}</Typography>
+          {" "}
+          <Typography
+            visibility={selectedOptions.length > 0 ? "visible" : "hidden"}
+          >
+            {title}
+          </Typography>
         </Box>
         <Select
           className="basic-multi-select"
+          placeholder={title}
           isMulti={multi}
           options={options}
+          onChange={(selected) => setSelectedOptions(selected)}
         />
       </FormControl>
     </Grid>
