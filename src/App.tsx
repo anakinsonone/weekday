f4e5d311177a78430a05ff95e46504f62e0e1049
akhileshsonone <akkhi.sonone@gmail.com>
2024-05-05 18:34:04 +0530

    implement job board

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,124 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Grid } from "@mui/material";
+
+import { JobDescription } from "./utils/types";
+import { Loader, JobCard, Filters } from "./components";
+
 const App = () => {
-  return <div>App</div>;
+  const [offset, setOffset] = useState(0);
+  const [currentJDs, setCurrentJDs] = useState<JobDescription[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const fetchInitialData = () => {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const body = JSON.stringify({
+        limit: 9,
+        offset: 0,
+      });
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body,
+      };
+
+      fetch(
+        "https://api.weekday.technology/adhoc/getSampleJdJSON",
+        requestOptions,
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setCurrentJDs(result.jdList);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setIsLoading(false);
+        });
+    };
+    fetchInitialData();
+  }, []);
+
+  const fetchAdditionalData = useCallback(() => {
+    if (isLoading) return;
+
+    setIsLoading(true);
+    const fetchData = () => {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const body = JSON.stringify({
+        limit: 9,
+        offset,
+      });
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body,
+      };
+
+      fetch(
+        "https://api.weekday.technology/adhoc/getSampleJdJSON",
+        requestOptions,
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setCurrentJDs((prevJDs) => [...prevJDs, ...result.jdList]);
+          setOffset((prevOffset) => prevOffset + 1);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setIsLoading(false);
+        });
+    };
+    fetchData();
+  }, [offset, isLoading]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        fetchAdditionalData();
+      }
+    });
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    const currRef = containerRef.current;
+    return () => {
+      if (currRef) {
+        observer.unobserve(currRef);
+      }
+    };
+  }, [fetchAdditionalData]);
+
+  return (
+    <div>
+      <Filters />
+      <Grid
+        container
+        columnSpacing={4}
+        rowSpacing={{ xs: 6, sm: 6, md: 4 }}
+        ref={containerRef}
+        className="jobcard-grid-container"
+      >
+        {currentJDs.map((jd, idx) => (
+          <JobCard key={idx} jobDescription={jd} />
+        ))}
+      </Grid>
+      <div ref={containerRef}>{isLoading && <Loader />}</div>
+    </div>
+  );
 };
 
 export default App;
